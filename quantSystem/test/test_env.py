e0feae3c29e3b1c7f1e91b947409a5e37b10317d
lzlzzZ <54053178+lzlzzZ@users.noreply.github.com>
2026-02-13 18:08:07 +0800

    Delete quantSystem directory

--- quantSystem/test/test_env.py
+++
@@ -1,9 +0,0 @@
-from quantSystem.data.fetcher import default_fetcher,StockDataFetcher
-
-class TestEnv:
-
-
-    df_000001 = default_fetcher.get_daily_data("000001", adjust="hfq")
-    if df_000001 is not None:
-        print(f"获取到 {len(df_000001)} 行数据")
-        print(df_000001[['open', 'close', 'volume']].head())
